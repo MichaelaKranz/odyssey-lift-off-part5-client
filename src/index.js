5248069d2ca5659540f3814d11cda215108bf9a1
Michaela Kranz <michaela.kranz@condat.de>
2022-04-29 14:44:08 +0200

    length entfernt

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  //uri: 'http://localhost:4000',
-  uri: 'https://server-catstronauts-mkr.herokuapp.com/',
+  uri: 'http://localhost:4000',
+  //uri: 'https://server-catstronauts-mkr.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
